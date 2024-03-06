fd345d2cc35b49555b7718ff9fda72b032131749
oitomato <125155607+MatheusKlein@users.noreply.github.com>
2024-03-06 12:24:43 -0300

    Add files via upload

---
+++ 0306/atv06032.php
@@ -0,0 +1,39 @@
+ 
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atividade avaliativa</title>
+    <link rel="stylesheet" href="aparencia.css">
+</head>
+<body>
+    <legend><b>Escolha uma função</b></legend>
+    <br>
+    <a href="abs.php">Absoluto</a><br>
+    <br>
+    <a href="base_convert.php">Conversão de base</a><br>
+    <br>
+    <a href="hypot.php">Hipotenusa</a><br>
+    <br>
+    <a href="intdiv.php">Divisão inteira</a><br>
+    <br>
+    <a href="min.php">Mínimo</a><br>
+    <br>
+    <a href="max.php">Máximo</a><br>
+    <br>
+    <a href="pi.php">PI</a><br>
+    <br>
+    <a href="pow.php">Potência</a><br>
+    <br>
+    <a href="sin.php">Seno</a><br>
+    <br>
+    <a href="cos.php">Cosseno</a><br>
+    <br>
+    <a href="tan.php">Tangente</a><br>
+    <br>
+    <a href="sqrt.php">Raiz Quadrada</a><br>
+    <br>
+
+</body>
+</html>
